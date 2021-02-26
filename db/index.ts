fb5ebc52ae7a7178567ad0e142c0609c2541d027
RJ Shoemaker <rjshoemaker55@gmail.com>
2021-02-25 21:21:29 -0500

    created index file for db

--- db/index.ts
+++ db/index.ts
@@ -1,1 +1,26 @@
 import { Sequelize } from 'sequelize-typescript'
+import pg from 'pg'
+import Goal from './models/Goal'
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'goalkeeper',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASS || 'postgres',
+  {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || 'postgres',
+    database: process.env.DB_NAME || 'goalkeeper',
+    host: process.env.DB_HOST || 'localhost',
+    storage: ':memory:',
+    models: [Goal],
+    dialect: 'postgres',
+    dialectModule: pg
+  }
+)
+
+const db = {
+  sequelize: sequelize,
+  Sequelize: Sequelize
+}
+
+export default db
